3190541f8d2c3933e3d4e29a020043b8e44e05c9
Administrator <2655158693@qq.com>
2020-06-30 11:56:04 +0800

    springboot使用redis

---
+++ RedisDemo/src/test/java/xyz/txcplus/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.txcplus.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
