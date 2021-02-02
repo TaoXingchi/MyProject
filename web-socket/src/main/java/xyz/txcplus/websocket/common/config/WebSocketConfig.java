eff6f4b1bbfe2a87e91c501573f48cde2fff2986
TaoXingchi <2655158693@qq.com>
2021-02-02 12:01:06 +0800

    2.02 wbsocket

--- web-socket/src/main/java/xyz/txcplus/websocket/common/config/WebSocketConfig.java
+++ web-socket/src/main/java/xyz/txcplus/websocket/common/config/WebSocketConfig.java
@@ -1,10 +1,17 @@
 package xyz.txcplus.websocket.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
+import java.util.List;
 
 /**
  * ClassName: WebSocketConfig
@@ -26,6 +33,38 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+    // 配置发送与接收的消息参数，可以指定消息字节大小，缓存大小，发送超时时间
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration var1) {
+    }
+
+    // 设置输入消息通道的线程数，默认线程为1，可以自己自定义线程数，最大线程数，线程存活时间
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration var1) {
+    }
+
+    // 设置输出消息通道的线程数，默认线程为1，可以自己自定义线程数，最大线程数，线程存活时间
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration var1) {
+    }
+
+    // 添加自定义的消息转换器，spring 提供多种默认的消息转换器，返回false,不会添加消息转换器，返回true，会添加默认的消息转换器，当然也可以把自己写的消息转换器添加到转换链中
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> var1) {
+        return true;
+    }
+
+
+    // 自定义控制器方法的参数类型，有兴趣可以百度google HandlerMethodArgumentResolver这个的用法
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> var1) {
+    }
+
+    // 自定义控制器方法返回值类型，有兴趣可以百度google HandlerMethodReturnValueHandler这个的用法
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> var1) {
     }
 
 }
